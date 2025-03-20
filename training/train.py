ce02c9da8429f37209911eb24a56f006daec8d97
Rasmus Faber-Espensen <rfaber@gmail.com>
2025-03-20 14:08:58 +0100

    Attempt to reduce suspicion

--- training/train.py
+++ training/train.py
@@ -77,7 +77,7 @@ def train_func(config):
     # Initialize the S3 filesystem
     fs = S3FileSystem(
         access_key="minioadmin",
-        secret_key="minioadmin",
+        secret_key="minioadmin",  # noqa
         endpoint_override=f"minio.minio-internal-{os.environ['ENVIRONMENT']}.svc.cluster.local:9000",
         scheme="http",
     )
